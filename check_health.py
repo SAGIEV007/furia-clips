f5ed214880a811344117a5bef1922db2176b6dc7
Fernando Da Conceicao Cordeiro Filho <70156213125@preventsenior.com.br>
2026-09-01 21:45:12 -0300

    chore: check_health.py verifica fontsdir e Montserrat-Bold.ttf

--- check_health.py
+++ check_health.py
@@ -1,13 +1,14 @@
+import os
 import app
 import sqlite3
 import config
 
-print(len(list(app.app.url_map.iter_rules())), 'rotas')
+routes = list(app.app.url_map.iter_rules())
+print(len(routes), 'rotas')
 
 c = sqlite3.connect(config.DB_PATH)
 print(c.execute('PRAGMA integrity_check').fetchone()[0])
 
-# Count projects, clips, transcriptions
 cur = c.cursor()
 cur.execute('SELECT COUNT(*) FROM projects')
 print('projects:', cur.fetchone()[0])
@@ -15,3 +16,9 @@ cur.execute('SELECT COUNT(*) FROM clips')
 print('clips:', cur.fetchone()[0])
 cur.execute('SELECT COUNT(*) FROM transcriptions')
 print('transcriptions:', cur.fetchone()[0])
+
+# Font/subtitle health
+fonts_dir = os.path.join(os.path.dirname(__file__), 'fonts')
+ttf = os.path.join(fonts_dir, 'Montserrat-Bold.ttf')
+print('fonts_dir_exists:', os.path.isdir(fonts_dir))
+print('montserrat_bold_exists:', os.path.isfile(ttf))
